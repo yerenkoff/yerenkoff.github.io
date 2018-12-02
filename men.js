687ae2a54dd39c83ba0c2fc0b98d8ea20eac8239
Kirill Yerenkoff <yerenkoff@gmail.com>
2018-12-02 05:16:50 +0500

    Initial commit

--- men.js
+++ men.js
@@ -1,22 +1,18 @@
-var showcaseImages = document.getElementById("showcaseImages");
-var arrowArray = [];
-var showcaseCounter = 1;
 var searchBar = document.getElementById("searchBar");
 var materialSearchIcon = document.getElementById("materialSearchIcon");
 var navigationBar = document.getElementById("navigationBar");
 var materialMenuIcon = document.getElementById("materialMenuIcon");
-var showcase__link = document.getElementsByClassName("showcase__link");
-var showcaseCircles = document.getElementsByClassName("showcaseCircles")[0].children;
-var circlesCounter = 0;
 var footerLists = document.getElementsByClassName("footer__list");
+var sortList = document.getElementsByClassName("sorting__sortNav")[0];
 var footerList;
 
-[].forEach.call(
-    document.getElementsByClassName("arrowButton"),
-    function(element) {
-        arrowArray.push(element);
+function showSort() {
+    if (sortList.style.maxHeight == 0 || sortList.style.maxHeight == "0px") {
+        sortList.style.maxHeight = "300px";
+    } else {
+        sortList.style.maxHeight = "0px";
     }
-);
+}
 
 var fadeIn = function() {
     var elements = document.getElementsByClassName("hidden");
@@ -134,84 +130,6 @@ function openList(param) {
         //     document.body.style.overflow = "visible";
         // }
     }
-}
-
-function setShowcaseBackground(panel) {
-    panel.style.backgroundImage = "url(img/showcase" + 1 + ".jpeg)";
-    showcase__link[0].textContent = "Incredddible!";
-    setTimeout(function() {
-        showcase__link[0].style.opacity = 1;
-    }, 400);
-    showcaseCircles[0].style.background = "#444";
-
-    arrowArray.forEach(function(element) {
-        element.onclick = function() {
-            panel.style.opacity = 0;
-            showcase__link[0].style.opacity = 0;
-            setTimeout(function() {
-                [].forEach.call(showcaseCircles, function(circle) {
-                    circle.style.background = "#eee";
-                });
-                if (element == arrowArray[0]) {
-                    if (showcaseCounter > 1) {
-                        panel.style.backgroundImage = "url(img/showcase" + (showcaseCounter - 1) + ".jpeg)";
-                        showcase__link[0].textContent = ((showcaseCounter - 1) == 1) ? "Incredddible!" : "Fishes in the RAIN!";
-                        setTimeout(function() {
-                            showcase__link[0].style.opacity = 1;
-                        }, 700);
-                        if (showcase__link[0].textContent == "Incredddible!") {
-                            showcaseCircles[0].style.background = "#444";
-                        } else {
-                            showcaseCircles[1].style.background = "#444";
-                        }
-                        showcaseCounter--;
-                        setTimeout(function() {
-                            panel.style.opacity = 1;
-                        }, 400);
-                    } else {
-                        showcaseCounter = 3;
-                        panel.style.backgroundImage = "url(img/showcase" + showcaseCounter + ".jpeg)";
-                        showcase__link[0].textContent = "Clouds in the HEAD!";
-                        showcaseCircles[2].style.background = "#444";
-                        setTimeout(function() {
-                            showcase__link[0].style.opacity = 1;
-                        }, 700);
-                        setTimeout(function() {
-                            panel.style.opacity = 1;
-                        }, 400);
-                    }
-                } else if (element == arrowArray[1]) {
-                    if (showcaseCounter < 3) {
-                        panel.style.backgroundImage = "url(img/showcase" + (showcaseCounter + 1) + ".jpeg)";
-                        showcase__link[0].textContent = ((showcaseCounter + 1) == 2) ? "Fishes in the RAIN!" : "Clouds in the HEAD!";
-                        setTimeout(function() {
-                            showcase__link[0].style.opacity = 1;
-                        }, 700);
-                        if (showcase__link[0].textContent == "Fishes in the RAIN!") {
-                            showcaseCircles[1].style.background = "#444";
-                        } else {
-                            showcaseCircles[2].style.background = "#444";
-                        }
-                        showcaseCounter++;
-                        setTimeout(function() {
-                            panel.style.opacity = 1;
-                        }, 400);
-                    } else {
-                        showcaseCounter = 1;
-                        panel.style.backgroundImage = "url(img/showcase" + showcaseCounter + ".jpeg)";
-                        showcase__link[0].textContent = "Incredddible!";
-                        showcaseCircles[0].style.background = "#444";
-                        setTimeout(function() {
-                            showcase__link[0].style.opacity = 1;
-                        }, 700);
-                        setTimeout(function() {
-                            panel.style.opacity = 1;
-                        }, 400);
-                    }
-                }
-            }, 400);
-        }
-    });
 }
 
 fadeIn();
